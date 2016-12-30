b2d48e0522c60800289d5cf359a09716575ef5fd
Guillermo Guirao Aguilar <contact@guillermoguiraoaguilar.com>
2016-12-30 20:21:03 +0100

    Added navbar

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,16 +1,12 @@
 var environment = require('../../config/environment');
 
 module.exports = function(req, res, next) {
-  if (environment !== 'development') {
-    if (req.isAuthenticated())
-      return next();
-    else if (req.url == '/')
-      res.redirect('/en');
-    else if (req.baseUrl == '/api')
-      res.status(401).send('Not authorized');
-    else
-      res.redirect('/login');
-  } else {
+  if (req.isAuthenticated())
     return next();
-  }
+  else if (req.url == '/')
+    res.redirect('/en');
+  else if (req.baseUrl == '/api')
+    res.status(401).send('Not authorized');
+  else
+    res.redirect('/login');
 };
--- app/routes/index.js
+++ app/routes/index.js
@@ -5,16 +5,21 @@ var api = require('./api');
 var login = require('./login');
 
 router.get('/', auth, function(req, res) {
-  res.render('main');
+  res.redirect('/dashboard');
 });
 
-router.get('/en', function(req, res) {
-  res.render('frontpage-en');
-})
+router.get(['/dashboard', '/settings'], auth, function(req, res) {
+  res.render(req.path.slice(1), {active: req.path.slice(1), user: req.user});
+});
 
-router.get('/de', function(req, res) {
-  res.render('frontpage-de');
-})
+router.get(['/en', '/de'], function(req, res) {
+  res.render('frontpage-' + req.path.slice(1));
+});
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
 
 router.use('/api', api);
 router.use('/login', login);
